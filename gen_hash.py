680944ce1a9e75f36cf8bce17ef73071a579b70b
0xff <126054063+Ooo0xffooO@users.noreply.github.com>
2023-05-07 14:36:06 +0800

    Update and rename gen_md5.py to gen_hash.py

--- gen_hash.py
+++ gen_hash.py
@@ -17,7 +17,15 @@ def read_file(file: Optional[Union[Path, str]]) -> bytes:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("file_path", help="file path")
+    parser.add_argument("-m", dest="mode", choices=["md5", "sha1", "sha256"], help="hash mode")
     args = parser.parse_args()
 
-    md5hasher = hashlib.md5(read_file(args.file_path))
-    print(md5hasher.hexdigest())
+    if args.mode == "md5":
+        md5hasher = hashlib.md5(read_file(args.file_path))
+        print(md5hasher.hexdigest())
+    elif args.mode == "sha1":
+        sha1hasher = hashlib.sha1(read_file(args.file_path))
+        print(sha1hasher.hexdigest())
+    elif args.mode == "sha256":
+        sha256hasher = hashlib.sha256(read_file(args.file_path))
+        print(sha256hasher.hexdigest())
